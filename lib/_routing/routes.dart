149c6ae815e809fb7be7e32be45dcb6fe580ff22
psu hack <bbc5256@psu.edu>
2020-07-12 16:43:23 -0400

    adding login route to routes.dart

--- lib/_routing/routes.dart
+++ lib/_routing/routes.dart
@@ -1,3 +1,4 @@
+import 'package:aidly/views/login.dart';
 import 'package:flutter/material.dart';
 import 'package:aidly/views/landing.dart';
 import 'package:aidly/_routing/routeNames.dart';
@@ -6,6 +7,8 @@ Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case landingViewRoute:
       return MaterialPageRoute(builder: (context) => LandingPage());
+    case loginViewRoute:
+      return MaterialPageRoute(builder: (context) => LoginPage());
 
     default:
       return MaterialPageRoute(builder: (context) => LandingPage());
