93bc2f200f70f550c645e1f9f00d058c1519a161
psu hack <bbc5256@psu.edu>
2020-08-09 16:27:33 -0400

    register thank you page now is scrollable

--- lib/views/registerThankYouPage.dart
+++ lib/views/registerThankYouPage.dart
@@ -22,103 +22,117 @@ class registerThankYouPage extends StatelessWidget {
       appBar: AppBar(title: Text('Thank You For Your Registration')),
       body: Container(
         margin: EdgeInsets.all(10.0),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                "Hi, ${model.firstName}\n\nPlease Verify your Information is correct:\n",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.teal,
-                  fontSize: 20.0,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Hi, ${model.firstName}\n\nPlease Verify your Information is correct:\n",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal,
+                    fontSize: 20.0,
+                  ),
                 ),
-              ),
-              SizedBox(height: 1),
-              Text('First Name:',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-              Text(model.firstName, style: TextStyle(fontSize: 22)),
-              Text('Last Name:',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-              Text(model.lastName, style: TextStyle(fontSize: 22)),
-              Text('Email:',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-              Text(model.email, style: TextStyle(fontSize: 22)),
-              Text('Password:',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-              Text(model.password, style: TextStyle(fontSize: 22)),
-              Text('Zip:',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-              Text(model.zip, style: TextStyle(fontSize: 22)),
-              Text('Phone:',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-              Text(model.phone, style: TextStyle(fontSize: 22)),
-              Text('Days Available:',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-              showDay(model.day),
-              Text('Time Available:',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-              showTime(model.time),
-              Text('Volunteer Statement:',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-              Text(model.statement, style: TextStyle(fontSize: 22)),
-              MaterialButton(
-                color: primaryColor,
-                onPressed: () {
-                  print(this.model.firstName);
-                  new Future(()=>null).then((value) => pushData(this.model)).then((value) {
-                    print(value);
-                    if(value['status'] == 'success') {
-                      Constants.prefs.setString('token', value['auth_token']);
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  InterestsPage(model: this.model)));
-                    } else {
-                      showDialog<Null>(
-                        context: context,
-                        barrierDismissible: false,
-                        builder: (BuildContext context) {
-                          return new AlertDialog(
-                            title: new Text('Registration Failed'),
-                            content: new SingleChildScrollView(
-                              child: new ListBody(
-                                children: <Widget>[
-                                  new Text('Sorry, your registration failed. Please try again.'),
-                                ],
+                SizedBox(height: 1),
+                Text('First Name:',
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                Text(model.firstName, style: TextStyle(fontSize: 22)),
+                Text('Last Name:',
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                Text(model.lastName, style: TextStyle(fontSize: 22)),
+                Text('Email:',
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                Text(model.email, style: TextStyle(fontSize: 22)),
+                Text('Password:',
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                Text(model.password, style: TextStyle(fontSize: 22)),
+                Text('Zip:',
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                Text(model.zip, style: TextStyle(fontSize: 22)),
+                Text('Phone:',
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                Text(model.phone, style: TextStyle(fontSize: 22)),
+                Text('Days Available:',
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                showDay(model.day),
+                Text('Time Available:',
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                showTime(model.time),
+                Text('Volunteer Statement:',
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                Text(model.statement, style: TextStyle(fontSize: 22)),
+                MaterialButton(
+                  color: primaryColor,
+                  onPressed: () {
+                    print(this.model.firstName);
+                    new Future(() => null)
+                        .then((value) => pushData(this.model))
+                        .then((value) {
+                      print(value);
+                      if (value['status'] == 'success') {
+                        Constants.prefs.setString('token', value['auth_token']);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    InterestsPage(model: this.model)));
+                      } else {
+                        showDialog<Null>(
+                          context: context,
+                          barrierDismissible: false,
+                          builder: (BuildContext context) {
+                            return new AlertDialog(
+                              title: new Text('Registration Failed'),
+                              content: new SingleChildScrollView(
+                                child: new ListBody(
+                                  children: <Widget>[
+                                    new Text(
+                                        'Sorry, your registration failed. Please try again.'),
+                                  ],
+                                ),
                               ),
-                            ),
-                            actions: <Widget>[
-                              new FlatButton(
-                                child: new Text('Confirm'),
-                                onPressed: () {
-                                  Navigator.of(context)..pop()..pop();
-                                },
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    }
+                              actions: <Widget>[
+                                new FlatButton(
+                                  child: new Text('Confirm'),
+                                  onPressed: () {
+                                    Navigator.of(context)..pop()..pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
 //                    Navigator.pushNamed(
 //                      (context),
 //                      '/interests',
 //                    );
-                  });
-                },
-                child: Center(
-                  child: Text(
-                    'Click to Continue',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      fontSize: 20.0,
-                      color: Colors.white,
+                    });
+                  },
+                  child: Center(
+                    child: Text(
+                      'Click to Continue',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -149,9 +163,9 @@ class registerThankYouPage extends StatelessWidget {
   }
 
   Future<Map<String, dynamic>> pushData(UserModel model) async {
-    Response res = await HttpRequests.postJson("http://165.227.87.42:1234/user/create", json.encode(model.toMap()));
+    Response res = await HttpRequests.postJson(
+        "http://165.227.87.42:1234/user/create", json.encode(model.toMap()));
     Map<String, dynamic> resJson = json.decode(res.body);
     return resJson;
   }
-
 }
